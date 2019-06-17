e6b6fe26de535bc87ea61d8328a451c35ee108ed
Craig Derington <craig@craigderington.me>
2019-06-17 10:21:01 -0400

    Update config and add new tasks.

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+from kombu import Queue, Exchange
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    SECRET_KEY = os.urandom(32)
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:$D1r3ct098!@localhost:3306/netauto'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', SQLITE_DB)
+
+    CELERY_TIMEZONE = 'US/Eastern'
+    CELERY_BROKER_URL = 'amqp://localhost/'
+    CELERY_RESULT_BACKEND = 'rpc://'
+
+    # define the tasks queues
+    CELERY_QUEUES = (
+        Queue('default', Exchange('default'), routing_key='default'),
+        Queue('messages', Exchange('messages'), routing_key='get_messages'),
+        Queue('locations', Exchange('locations'), routing_key='locations'),
+        Queue('devices', Exchange('devices'), routing_key='devices')
+    )
+
+    # define the task routes
+    CELERY_ROUTES = {
+        'get_new_messages': {'queue': 'get_messages', 'routing_key': 'get_messages'},
+        'get_all_locations': {'queue': 'get_locations', 'routing_key': 'get_locations'},
+        'get_all_devices': {'queue': 'get_devices', 'routing_key': 'get_devices'}
+    }
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MONGO_SERVER = 'server-path'
+    MONGO_DB = ''
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
--- netauto/tasks.py
+++ netauto/tasks.py
@@ -36,7 +36,7 @@ def convert_utc_to_local(utcdate_obj):
     return utcdate_obj + offset
 
 
-@celery.task(queue="get_messages", max_retires=3)
+@celery.task(queue="messages", max_retires=3)
 def log(message):
     """Print some log messages"""
     logger.debug(message)
@@ -51,15 +51,52 @@ def get_locations():
     :return list
     """
 
-    pathParams = "/locations"
+    resource_path = "/locations"
 
     try: 
         # make the request
         r = requests.request(
                 API_METHOD, 
-                SNOW_BASE_URL + pathParams, 
+                SNOW_BASE_URL + resource_path, 
                 auth=auth, 
                 headers=hdrs
+        )
+
+        # check for HTTP status codes other than 200
+        if r.status_code != 200: 
+            logger.warning('Status:', r.status_code, 'Headers:', r.headers, 'Error Response:', r.json())
+
+        # decode the JSON response into a dictionary and use the data
+        data = r.json()
+        logger.info(data)
+
+    except requests.HTTPError as http_err:
+        logger.warning("API call returned error: {}".format(str(http_err)))
+
+
+@celery.task(queue="locations", max_retires=3)
+def get_location_info(id):
+    """
+    Get each location information by Store
+    :param: id (int)
+    :return json object
+    """
+    if not isinstance(id, int):
+        try:
+            id = int(id)
+        except TypeError as err:
+            logger.critical("The store ID parameter is an invalid type and can not be corecred to integer: {}".format(str(err)))
+            return id
+
+    pathParams = id
+    resource_Path = "/locations/{}".format(str(pathParams))
+    
+    try:
+        r = requests.request(
+            API_METHOD, 
+            SNOW_BASE_URL + resource_Path, 
+            headers=hdrs, 
+            auth=auth
         )
 
         # check for HTTP status codes other than 200
